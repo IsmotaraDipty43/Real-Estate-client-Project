d4313ff0e9d8ae68f66df52fe726d56b392b21ca
IsmotaraDipty3243 <ismotaradipty81@gmail.com>
2025-01-17 23:09:11 +0600

    7th

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -7,10 +7,10 @@ const useAdmin = () => {
   const axiossecure = useAxiosSecure();
 
   const { data: isAdmin, isPending: isAdminLoading } = useQuery({
-    queryKey: [user?.email?.toLowerCase(), 'isAdmin'], // Convert email to lowercase for the query key
-    enabled: !!user?.email && !loading, // Ensure email exists and loading is complete
+    queryKey: [user?.email?.toLowerCase(), 'isAdmin'],
+    enabled: !!user?.email && !loading, 
     queryFn: async () => {
-      const emailLower = user.email.toLowerCase(); // Convert email to lowercase
+      const emailLower = user.email.toLowerCase(); 
       const res = await axiossecure.get(`/users/adminuser/${emailLower}`);
       console.log(res.data); // Check the response data
       return res.data?.admin; // Return whether the user is an admin
