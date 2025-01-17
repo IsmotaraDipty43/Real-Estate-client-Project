5f83a515a65fc21103cd69cc0f1035d54e2cdb78
IsmotaraDipty3243 <ismotaradipty81@gmail.com>
2025-01-17 23:10:14 +0600

    8th

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
 
-// Create the axios instance outside of the hook to avoid re-creation on every render
+
 const axiosSecure = axios.create({
   baseURL: 'https://realstate-server-rouge.vercel.app',
   
@@ -12,12 +12,9 @@ const axiosSecure = axios.create({
 const useAxiosSecure = () => {
   const navigate = useNavigate();
   const { logOut } = useContext(AuthContext);
-
-  // Request interceptor to attach the token
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem('access-token');
-      // console.log('Request intercepted, token:', token);
       if (token) {
         config.headers.authorization = `Bearer ${token}`;
       }
@@ -27,11 +24,9 @@ const useAxiosSecure = () => {
       return Promise.reject(error);
     }
   );
-
-  // Response interceptor to handle 401/403 errors
   axiosSecure.interceptors.response.use(
     function (response) {
-      return response; // Simply return the response for successful requests
+      return response; 
     },
     async (error) => {
       const status = error.response?.status;
